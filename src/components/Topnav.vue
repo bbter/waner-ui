98736edb0fe0de1861c16010acb1488e86f65647
bbter <1035675608@qq.com>
2020-10-14 16:44:03 +0800

    实现aside切换

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,40 +1,47 @@
 <template>
-<div class="topnav">
-    <div class="logo">LOGO</div>
+  <div class="topnav">
+    <div class="logo" @click="toggleAside">LOGO</div>
     <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
+      <li>菜单1</li>
+      <li>菜单2</li>
     </ul>
-</div>
+  </div>
 </template>
 
 <script lang="ts">
+import { inject, Ref } from "vue";
 export default {
-
-}
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("asideVisible");
+    const toggleAside = () => {
+        asideVisible.value = !asideVisible.value
+    }
+    return {toggleAside}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .topnav {
-    background: pink;
+  background: pink;
+  display: flex;
+  padding: 16px;
+  position: relative;
+  z-index: 10;
+
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+
+  > .menu {
     display: flex;
-    padding: 16px;
-    position: relative;
-    z-index: 10;
+    white-space: nowrap;
+    flex-wrap: nowrap;
 
-    >.logo {
-        max-width: 6em;
-        margin-right: auto;
+    > li {
+      margin: 0 1em;
     }
-
-    >.menu {
-        display: flex;
-        white-space: nowrap;
-        flex-wrap: nowrap;
-
-        >li {
-            margin: 0 1em;
-        }
-    }
+  }
 }
 </style>
