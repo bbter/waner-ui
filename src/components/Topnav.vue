a0b88e672e2123c5f239b17b2091c9a2bc779930
bbter <1035675608@qq.com>
2020-10-14 16:49:42 +0800

    让Topnav是响应式的

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -5,6 +5,7 @@
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside"></span>
   </div>
 </template>
 
@@ -28,7 +29,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
-
+  justify-content: center;
+  align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -42,6 +44,13 @@ export default {
     > li {
       margin: 0 1em;
     }
+  }
+  > .toggleAside{
+      
+  }
+  @media (max-width: 500px) {
+      > .menu{display: none;}
+      > .logo{margin: 0 auto;}
   }
 }
 </style>
