b0e217ed000774e85d73d93092f69cd774f5a1fd
bbter <1035675608@qq.com>
2020-10-14 13:39:28 +0800

    搭建HTML结构

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
-<div>导航栏 |
-    <router-link to="/">Bbt</router-link> |
-    <router-link to="/xxx">Bbt2</router-link>
-</div>
-<hr>
 <router-view></router-view>
 </template>
 
 <script>
 export default {
-    name: 'App',
-}
+    name: "App",
+};
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import {createWebHashHistory,createRouter} from 'vue-router'
-import Bbt from './components/Bbt.vue'
-import Bbt2 from './components/Bbt2.vue'
+import { createWebHashHistory, createRouter } from 'vue-router'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
-    history:history,
-    routes:[
-        {path:'/',component:Bbt},
-        {path:'/xxx',component:Bbt2}
+    history: history,
+    routes: [
+        { path: '/', component: Home },
+        { path: '/doc', component: Doc }
     ]
 })
 
