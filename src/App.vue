cdab6295daac4b7efc8b456f7eaafc7c1ebd30e6
bbter <1035675608@qq.com>
2020-10-14 12:57:29 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div>hi</div>
+<Bbt></Bbt>
+</template>
+
+<script>
+import Bbt from './components/Bbt.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Bbt
+    }
+}
+</script>
