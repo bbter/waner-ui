2a7494813cb2e01e384b1e08ab43e7c194726d53
bbter <1035675608@qq.com>
2020-10-15 18:08:23 +0800

    Tab CSS 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,24 +1,62 @@
 <template>
-    <div>
-        <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-        <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+  <div class="waner-tabs">
+    <div class="waner-tabs-nav">
+      <div class="waner-tabs-nav-item" v-for="(t, index) in titles" :key="index">
+        {{ t }}
+      </div>
     </div>
+    <div class="waner-tabs-content">
+      <component
+        class="waner-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Tab from './Tab.vue'
+import Tab from "./Tab.vue";
 export default {
-    setup(props,context){
-        const defaults = context.slots.default()
-        defaults.forEach((tag)=>{
-            if(tag.type !== Tab){
-                throw new Error('Tabs 子标签必须是Tab')
-            }
-        })
-        const titles = defaults.map((tag)=>{
-            return tag.props.title
-        })
-        return {defaults,titles}
-    }
-}
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs 子标签必须是Tab");
+      }
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    return { defaults, titles };
+  },
+};
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.waner-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
