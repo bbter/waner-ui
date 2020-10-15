0994db4402b5975d119fcf8bd2ff55cc673ef0d0
bbter <1035675608@qq.com>
2020-10-15 09:51:51 +0800

    switch css 2

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,20 +1,20 @@
 <template>
-  <button @click="toggle" :class="{checked:value}"><span></span></button>
+  <button @click="toggle" :class="{ checked: value }"><span></span></button>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 export default {
-    props:{
-        value: Boolean
-    },
-    setup(props,context) {
-        const toggle = () => {
-            context.emit('update:value',!props.value)
-        }
-        return {toggle}
-    }
-}
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
+    const toggle = () => {
+      context.emit("update:value", !props.value);
+    };
+    return { toggle };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -24,27 +24,38 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: grey;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
 }
-span{
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background:white;
-    border-radius: $h2 / 2;
-    transition: left 250ms;
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: $h2 / 2;
+  transition: all 250ms;
 }
-button.checked{
-    background: blue;
+button.checked {
+  background: #1890ff;
 }
-button.checked > span{
-    left:calc(100% - #{$h2} - 2px)
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
 }
-button:focus{
-    outline: none;
+button:focus {
+  outline: none;
+}
+button:active {
+  > span {
+    width: $h2 + 4px;
+  }
+}
+button.checked:active {
+  > span {
+    width: $h2 + 4px;
+    margin-left: -4px;
+  }
 }
 </style>
