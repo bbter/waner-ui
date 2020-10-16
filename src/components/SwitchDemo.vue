6fed77f1414aa023f64b7b8fd075ed93d3e70235
bbter <1035675608@qq.com>
2020-10-16 13:22:57 +0800

    展示源代码并且高亮

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,8 +10,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{Switch1Demo.__sourceCode}}</pre>
-      </div>
+        <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode,Prism.languages.html,'html')" />
     </div>
     <div class="demo">
       <h2>支持 disabled</h2>
@@ -22,7 +21,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{Switch2Demo.__sourceCode}}</pre>
+        <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode,Prism.languages.html,'html')" />
       </div>
     </div>
   </div>
@@ -34,12 +33,15 @@ import Switch from "../lib/Switch.vue";
 import Button from "../lib/Button.vue";
 import Switch1Demo from "./Switch1.demo.vue";
 import Switch2Demo from "./Switch2.demo.vue";
+import 'prismjs'
+import 'prismjs/themes/prism-okaidia.css'
+const Prism = (window as any).Prism
 import { ref } from "vue";
 export default {
   components: {Button },
   setup() {
     const bool = ref(false);
-    return { bool,Switch1Demo,Switch2Demo };
+    return { bool,Switch1Demo,Switch2Demo,Prism };
   },
 };
 </script>
