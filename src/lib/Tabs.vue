cab692a384235b807db3febd2caf6e24771a249c
bbter <1035675608@qq.com>
2020-10-16 17:39:44 +0800

    优化Tabs.vue

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,79 +1,75 @@
 <template>
-  <div class="waner-tabs">
-    <div class="waner-tabs-nav" ref="container">
-      <div
-        class="waner-tabs-nav-item"
-        :ref="
-          (el) => {
-            if (t === selected) selectedItem = el;
-          }
-        "
-        :class="{ selected: t === selected }"
-        @click="select(t)"
-        v-for="(t, index) in titles"
-        :key="index"
-      >
-        {{ t }}
-      </div>
-      <div class="waner-tabs-nav-indicator" ref="indicator"></div>
-    </div>
-    <div class="waner-tabs-content">
-      <component
-        :class="{ selected: c.props.title === selected }"
-        v-for="(c, index) in defaults"
-        :key="index"
-        class="waner-tabs-content-item"
-        :is="c"
-      />
-    </div>
+<div class="waner-tabs">
+  <div class="waner-tabs-nav" ref="container">
+    <div class="waner-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+    <div class="waner-tabs-nav-indicator" ref="indicator"></div>
   </div>
+  <div class="waner-tabs-content">
+    <component :is="current" :key="current.props.title" />
+  </div>
+</div>
 </template>
 
 <script lang="ts">
-import { computed, onMounted, ref, watchEffect } from "vue";
-import Tab from "./Tab.vue";
+import Tab from './Tab.vue'
+import {
+  computed,
+  ref,
+  watchEffect,
+  onMounted
+} from 'vue'
 export default {
   props: {
     selected: {
-      type: String,
-    },
+      type: String
+    }
   },
   setup(props, context) {
-    const defaults = context.slots.default();
-    const selectedItem = ref<HTMLDivElement>(null);
-    const indicator = ref<HTMLDivElement>(null);
-    const container = ref<HTMLDivElement>(null);
+    const selectedItem = ref < HTMLDivElement > (null)
+    const indicator = ref < HTMLDivElement > (null)
+    const container = ref < HTMLDivElement > (null)
     onMounted(() => {
       watchEffect(() => {
-        const { width } = selectedItem.value.getBoundingClientRect();
-        indicator.value.style.width = width + "px";
-        const { left: left1 } = container.value.getBoundingClientRect();
-        const { left: left2 } = selectedItem.value.getBoundingClientRect();
-        const left = left2 - left1;
-        indicator.value.style.left = left + "px";
-      });
-    });
+        const {
+          width
+        } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect()
+        const {
+          left: left2
+        } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      },{flush:'post'})
+    })
+    const defaults = context.slots.default()
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
-        throw new Error("Tabs 子标签必须是Tab");
+        throw new Error('Tabs 子标签必须是 Tab')
       }
-    });
+    })
+    const current = computed(() => {
+      return defaults.find(tag => tag.props.title === props.selected)
+    })
     const titles = defaults.map((tag) => {
-      return tag.props.title;
-    });
+      return tag.props.title
+    })
     const select = (title: string) => {
-      context.emit("update:selected", title);
-    };
+      context.emit('update:selected', title)
+    }
     return {
+      current,
       defaults,
       titles,
       select,
       selectedItem,
       indicator,
-      container,
-    };
-  },
-};
+      container
+    }
+  }
+}
 </script>
 
 <style lang="scss">
@@ -109,12 +105,6 @@ $border-color: #d9d9d9;
   }
   &-content {
     padding: 8px 0;
-    &-item {
-      display: none;
-      &.selected {
-        display: block;
-      }
-    }
   }
 }
 </style>
