efebf68ac71dc0f81e019ab6b7d97478638d2a6e
bbter <1035675608@qq.com>
2020-10-16 11:28:19 +0800

    引入github 样式

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        Intro
-    </div>
+  <article class="markdown-body">
+    <h1>介绍</h1>
+    <p>Waner UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+    <p>
+      这款组件库其实是我为了学习Vue
+      3，全程亲手编写，尽量不采用第三方库，包括你现在看到的这个官网也几乎都是我自己写的。
+    </p>
+    <p>
+      所以如果强烈不建议你将此 UI
+      库用于生产环境。但如果你是抱着看源代码的目的来的，那么这个库还是值得一看的。
+    </p>
+    下一节：<a href="#/doc/install">安装</a>
+  </article>
 </template>
