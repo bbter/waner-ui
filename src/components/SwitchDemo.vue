eb2cd37ff3f3c8bfa16ffa12433cf02b21286d56
bbter <1035675608@qq.com>
2020-10-16 13:55:11 +0800

    删除无用代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -8,20 +8,14 @@
 
 
 <script lang="ts">
-import Switch from "../lib/Switch.vue";
 import Switch1Demo from "./Switch1.demo.vue";
 import Switch2Demo from "./Switch2.demo.vue";
 import Demo from "./Demo.vue"
-import Button from "../lib/Button.vue";
-import 'prismjs'
-import 'prismjs/themes/prism-okaidia.css'
-const Prism = (window as any).Prism
 import { ref } from "vue";
 export default {
-  components: {Button,Demo },
+  components: {Demo },
   setup() {
-    const bool = ref(false);
-    return { bool,Switch1Demo,Switch2Demo,Prism };
+    return {Switch1Demo,Switch2Demo};
   },
 };
 </script>
